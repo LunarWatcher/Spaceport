5c5559fa60a71feee60d576c329e0624337f8fd6
Olivia <zoe.i2k1@gmail.com>
2022-11-23 02:20:28 +0100

    I suspect the CI is too slow to keep up with 100ms timeout

--- tests/src/ServerTests.cpp
+++ tests/src/ServerTests.cpp
@@ -7,13 +7,13 @@
 TEST_CASE("Test index works", "[ServerBase]") {
     tests::PortMock mock;
 
-    auto r = cpr::Get(cpr::Url{"localhost:1337/"}, cpr::Timeout{100});
+    auto r = cpr::Get(cpr::Url{"localhost:1337/"}, cpr::Timeout{1000});
     REQUIRE(r.status_code == 200);
     REQUIRE(r.text.find("<body>") != std::string::npos);
 
     mock.stop();
     // NOTE: There has to be a timeout. Unix socket fuckery forces
     // this to halt because the socket is reserved, but the server is unresponsive
-    auto r2 = cpr::Get(cpr::Url{"localhost:1337"}, cpr::Timeout{100});
+    auto r2 = cpr::Get(cpr::Url{"localhost:1337"}, cpr::Timeout{1000});
     REQUIRE(r2.status_code == 0);
 }
